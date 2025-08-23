226b9f7b241f0f13c249e86733ed1e9b192896f0
kalairna <slxrddd@gmail.com>
2025-08-23 09:10:16 +0200

    Add files via upload

---
+++ currentVersion.lua
@@ -0,0 +1,51 @@
+-- L9Engine Version Control
+-- Ce fichier contient la liste de tous les champions disponibles avec leurs versions
+
+Data = {
+    Core = {
+        Version = 1.0
+    },
+    Champions = {
+        Sylas = { Version = 1.0 },
+        Pyke = { Version = 1.0 },
+        Aurora = { Version = 1.0 },
+        Draven = { Version = 1.0 },
+        MasterYi = { Version = 1.0 },
+        Thresh = { Version = 1.0 },
+        Kayn = { Version = 1.0 },
+        Rengar = { Version = 1.0 },
+        Ahri = { Version = 1.0 },
+        Yasuo = { Version = 1.0 },
+        Zed = { Version = 1.0 },
+        Lux = { Version = 1.0 },
+        Morgana = { Version = 1.0 },
+        Jinx = { Version = 1.0 },
+        Vayne = { Version = 1.0 },
+        LeeSin = { Version = 1.0 },
+        Katarina = { Version = 1.0 },
+        Darius = { Version = 1.0 },
+        Garen = { Version = 1.0 },
+        Jhin = { Version = 1.0 },
+        Kaisa = { Version = 1.0 },
+        Lucian = { Version = 1.0 },
+        MissFortune = { Version = 1.0 },
+        Nautilus = { Version = 1.0 },
+        Orianna = { Version = 1.0 },
+        Pantheon = { Version = 1.0 },
+        Riven = { Version = 1.0 },
+        Senna = { Version = 1.0 },
+        Sett = { Version = 1.0 },
+        Sona = { Version = 1.0 },
+        Soraka = { Version = 1.0 },
+        Syndra = { Version = 1.0 },
+        Tristana = { Version = 1.0 },
+        Twitch = { Version = 1.0 },
+        Varus = { Version = 1.0 },
+        Veigar = { Version = 1.0 },
+        Velkoz = { Version = 1.0 },
+        Warwick = { Version = 1.0 },
+        Xerath = { Version = 1.0 },
+        Yone = { Version = 1.0 },
+        Zoe = { Version = 1.0 }
+    }
+}
